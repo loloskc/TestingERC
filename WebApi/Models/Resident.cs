603a41805ab93f04cc3b9c108768bdda9760c69c
Denis <denishome21@gmail.com>
2024-03-05 21:50:44 +0500

    Добавьте файлы проекта.

---
+++ WebApi/Models/Resident.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi.Models
+{
+    public class Resident
+    {
+        public int Id { get; set; }
+        public string FIO { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
