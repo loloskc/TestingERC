c0823bf108519b8927c128bea7595544eae0e2a2
Denis <denishome21@gmail.com>
2024-03-05 21:57:00 +0500

    add context database

---
+++ WebApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public DbSet<Resident> Residents { get; set; }
+        public DbSet<BankBook> BankBooks { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) 
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
