f979bf71b31177485b8a39b701c573517bfa3601
Denis <denishome21@gmail.com>
2024-03-05 21:52:40 +0500

    add model bank book

--- WebApi/Models/BankBook.cs
+++ WebApi/Models/BankBook.cs
@@ -1,7 +1,16 @@
-﻿namespace WebApi.Models
+﻿using System;
+
+namespace WebApi.Models
 {
     public class BankBook
     {
-
+        public int Id { get; set; }
+        public string Number { get; set; }
+        public DateTime DateStart { get; set; }
+        public DateTime DateEnd { get; set; }
+        public string Address { get; set; }
+        public int Sqaure { get; set; }
+        public int ResidentId { get; set; }
+        public Resident Resident { get; set; }
     }
 }
