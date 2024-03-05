5771c956a99fc00c65a44f0b3f8c9db63f4a049e
Denis <denishome21@gmail.com>
2024-03-06 04:58:08 +0500

    фывф

---
+++ Client/Controllers/BankBookController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Controllers
+{
+    public class BankBookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Client/Startup.cs
+++ Client/Startup.cs
@@ -3,7 +3,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.HttpsPolicy;
 
 namespace Client
 {
